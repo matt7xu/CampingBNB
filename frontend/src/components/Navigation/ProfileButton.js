8a0d09b0cc4b3092015d5f7b9a351b940ee6d9ec
lingxuan xu <matt7xu@gmail.com>
2023-05-27 12:03:21 -0700

    ProfileButton component

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,9 +1,37 @@
-const ProfileButton = () => {
+import React, { useState } from "react";
+import { useDispatch } from 'react-redux';
+import * as sessionActions from '../../store/session';
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [open, setOpen] = React.useState(false);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div style={{ color: "orange", fontSize: "100px" }}>
-      <i class="fa-solid fa-id-card"></i>
+    <div className="dropdown">
+      <button onClick={handleOpen}>
+        {/* <i className="fas fa-user-circle" /> */}
+        <i className="fas fa-id-card" />
+      </button>
+      {open ? (<ul className="profile-dropdown">
+        <li>Username: {user.username}</li>
+        <li>Name: {user.firstName} {user.lastName}</li>
+        <li>Email: {user.email}</li>
+        {/* <li>
+          <button onClick={logout}>Log Out
+          </button>
+        </li> */}
+      </ul>) : null}
     </div>
   );
-};
+}
 
 export default ProfileButton;
