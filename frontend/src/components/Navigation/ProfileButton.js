398bc2dbecfb3971e39b46383a3ac5c3160be6a3
lingxuan xu <matt7xu@gmail.com>
2023-05-27 10:43:54 -0700

    Navigation component

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,9 @@
+const ProfileButton = () => {
+  return (
+    <div style={{ color: "orange", fontSize: "100px" }}>
+      <i class="fa-solid fa-id-card"></i>
+    </div>
+  );
+};
+
+export default ProfileButton;
