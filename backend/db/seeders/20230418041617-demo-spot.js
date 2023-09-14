7dd9bd91664c4f2422399908767e1fd881d3a26c
lingxuan xu <matt7xu@gmail.com>
2023-09-13 20:42:39 -0700

    styleing2

--- backend/db/seeders/20230418041617-demo-spot.js
+++ backend/db/seeders/20230418041617-demo-spot.js
@@ -21,8 +21,8 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         ownerId: 1,
-        address: "123 A St",
-        city: "San Francisco",
+        address: "26 Grove St",
+        city: "Half Moon Bay",
         state: "California",
         country: "United States of America",
         lat: 37.7645358,
@@ -35,85 +35,85 @@ module.exports = {
       },
       {
         ownerId: 1,
-        address: "123 B St",
-        city: "San Francisco",
+        address: "120 Central Ave",
+        city: "Half Moon Bay",
         state: "California",
         country: "United States of America",
         lat: 37.7645356,
         lng: -122.4730325,
-        name: "B Academy",
-        description: "Place where web developers are created",
-        price: 2123,
+        name: "Groveland Great Glamping",
+        description: "Enjoy the lovely setting of this romantic spot in nature. ",
+        price: 212,
         createdAt: "2021-11-19 20:39:36",
         updatedAt: "2021-11-19 20:39:36"
       },
       {
         ownerId: 3,
-        address: "123 C St",
-        city: "San Francisco",
+        address: "120 Myrtle St",
+        city: "Half Moon Bay",
         state: "California",
         country: "United States of America",
         lat: 37.7645357,
         lng: -122.4730326,
-        name: "C Academy",
-        description: "Place where web developers are created",
-        price: 3123,
+        name: "The Invisible Home",
+        description: "Welcome to our off-grid tiny home near the Grand Canyon, where nature takes center stage!",
+        price: 312,
         createdAt: "2021-11-19 20:39:36",
         updatedAt: "2021-11-19 20:39:36"
       },
       {
         ownerId: 1,
-        address: "123 D St",
-        city: "San Francisco",
+        address: "120 Poplar St",
+        city: "Half Moon Bay",
         state: "California",
         country: "United States of America",
         lat: 37.7645357,
         lng: -122.4730326,
-        name: "D Academy",
-        description: "Place where web developers are created",
-        price: 4123,
+        name: "Cascade Camp Surrounded by National Forest",
+        description: "Experience the beauty of nature at this unforgettable escape. ",
+        price: 412,
         createdAt: "2021-11-19 20:39:36",
         updatedAt: "2021-11-19 20:39:36"
       },
       {
         ownerId: 2,
-        address: "123 E St",
-        city: "San Francisco",
+        address: "123 Miramontes Ave",
+        city: "Half Moon Bay",
         state: "California",
         country: "United States of America",
         lat: 37.7645357,
         lng: -122.4730326,
-        name: "E Academy",
-        description: "Place where web developers are created",
-        price: 5123,
+        name: "Ocean Breeze - Camping",
+        description: "Incorporating the magnificent beauty of the Stinson beach & our cool glamping/cottage nestled at the most sought-after location, we have created a  truly versatile holiday spot. ",
+        price: 512,
         createdAt: "2021-11-19 20:39:36",
         updatedAt: "2021-11-19 20:39:36"
       },
       {
         ownerId: 3,
-        address: "123 F St",
+        address: "123 Pine Ave",
         city: "San Francisco",
         state: "California",
         country: "United States of America",
         lat: 37.7645357,
         lng: -122.4730326,
-        name: "F Academy",
-        description: "Place where web developers are created",
-        price: 6123,
+        name: "Crosley - Private Glamping",
+        description: "The Crosley Bell Tent: Your Private Oasis in Northern California",
+        price: 123,
         createdAt: "2021-11-19 20:39:36",
         updatedAt: "2021-11-19 20:39:36"
       },
       {
         ownerId: 1,
-        address: "123 G St",
+        address: "123 Kelly Ave",
         city: "San Francisco",
         state: "California",
         country: "United States of America",
         lat: 37.7645357,
         lng: -122.4730326,
-        name: "G Academy",
-        description: "Place where web developers are created",
-        price: 7123,
+        name: "Camping fun by the lake",
+        description: "Reconnect with nature at this unforgettable escape.",
+        price: 713,
         createdAt: "2021-11-19 20:39:36",
         updatedAt: "2021-11-19 20:39:36"
       }
@@ -130,7 +130,7 @@ module.exports = {
     options.tableName = 'Spots';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      name: { [Op.in]: ["A Academy","B Academy","C Academy","D Academy","E Academy","F Academy","G Academy"] }
+      name: { [Op.in]: ["The Loon Tent at Salmonier River Tents","Groveland Great Glamping","The Invisible Home","Cascade Camp Surrounded by National Forest","Ocean Breeze - Camping","Crosley - Private Glamping","Camping fun by the lake"] }
     }, {});
   }
 };
